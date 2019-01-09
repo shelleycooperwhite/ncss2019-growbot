c0e27616bb6b4fb14fa508cb8d168c809cda7d99
Shelley Cooper-White <hello@shelley.io>
2019-01-10 00:14:00 +1100

    alexa be better

--- alexa.py
+++ alexa.py
@@ -8,14 +8,24 @@ context = {}
 
 @app.route('/alexa', methods=['POST', 'GET'])
 def alexa():
+  payload = request.get_json()
+
+  request_type = payload['request']['type']
+  if request_type == 'IntentRequest':
+    query = payload['request']['intent']['slots']['query']['value']
+    response_text = 'You said: ' + query
+  else:
+    response_text = 'Say something!'
+
   return jsonify({
     'version': '0.1',
     'response': {
       'outputSpeech': {
         'type': 'PlainText',
-        'text': 'Hello, welcome to my bot'
+        'text': response_text
       }
-    }
+    },
+    'shouldEndSession': False,
   })
 
 if __name__ == '__main__':
