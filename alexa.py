7e488b85c156018af727723bd1c9cd27ff75bbdb
Shelley Cooper-White <hello@shelley.io>
2019-01-10 00:49:26 +1100

    give alex convo a red hot go

--- alexa.py
+++ alexa.py
@@ -8,23 +8,47 @@ context = {}
 
 @app.route('/alexa', methods=['POST', 'GET'])
 def alexa():
+  global state, context
   payload = request.get_json()
   print(payload)
 
-  request_type = payload['request']['type']
-  if request_type == 'IntentRequest':
-    query = payload['request']['intent']['slots']['query']['value']
-    print(query)
-    response_text = 'You said: ' + query
-  else:
-    response_text = 'Say something!'
+  request_type = None
+  if payload['request']:
+    request_type = payload['request']['type']
+
+  try:
+    request_query = payload['request']['intent']['slots']['query']['value']
+  except KeyError:
+    request_query = ''
+
+  # We are starting, so we should reset the state
+  if request_type == 'LaunchRequest':
+    state = 'NO_QUERY'
+    context = None
+    print('Launched, resetting state')
+
+  elif request_type == 'IntentRequest':
+    print(f'Processing {request_query} with ({state}, {context})')
+
+    # Intents will have some input, so we need to process it
+    # Change the conversation state based on the message from the user
+
+    state, context, output = on_input(state, context, request_query)
+    print(f'Result ({state}, {data})')
+
+  # Do something based on the state
+  print(f'Starting on enter ({state}, {data})')
+  on_enter_state(state, context)
+
+  # Build our response
+  print(f'Giving response {output}')
 
   return jsonify({
     'version': '0.1',
     'response': {
       'outputSpeech': {
         'type': 'PlainText',
-        'text': response_text
+        'text': output
       },
       'shouldEndSession': False,
     },
