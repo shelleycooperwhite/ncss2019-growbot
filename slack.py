ce3984e5e9c78bfc6fca65e8799f9ac07ab87259
Shelley Cooper-White <hello@shelley.io>
2019-01-09 13:25:43 +1100

    Make slackbot actually work and also good

--- slack.py
+++ slack.py
@@ -10,20 +10,22 @@ context = {}
 def slack_event():
   global state, context
 
-  payload = request.get_json()
+  payload = request.values
   print(payload)
 
   if payload:
-    event = payload.get('event')
+    text = payload.get('text')
 
     # While state is not EXIT, talk to the user.
     if state != 'END':
-      state, data, output1 = on_input(state, event.get('text'), context)
+      state, data, output1 = on_input(state, text, context)
 
       # Enter the new state.
       output2 = on_enter_state(state, context)
 
-      return '{}\n{}'.format(output1, output2)
+      return '\n'.join([x for x in [output1, output2] if x is not None])
+
+  return ''
 
 if __name__ == '__main__':
   app.run()
